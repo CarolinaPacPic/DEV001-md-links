4f9787c24e6ce826d38540f42c1d2d6995d6d15b
Carolina Pacori <carolina.pacori@gmail.com>
2023-01-23 09:00:29 -0500

    ultima version de diagrama de flujo

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 const fs = require('fs');
 
-const mdLinks = (path, options) => {
-return new Promise((resolve, reject) => {
+const mdLinks = (path, options) => new Promise((resolve, reject) => {
   // identifica si la ruta existe.
-  if (fs.existsSync(path)){
-    //Chequear o convertir a una ruta absoluta
+  if (fs.existsSync(path)) {
+    // Chequear o convertir a una ruta absoluta
     // Probar si esa ruta absoluta es un archivo (o un directorio CREO Q NO)
-    //(Si es un directorio filtrar los archivos md.(CREO Q NO VA))
-    // Si es un archivo devuelve un arreglo 
+    // (Si es un directorio filtrar los archivos md.(CREO Q NO VA))
+    // Si es un archivo devuelve un arreglo
 
-  }else {
+  } else {
   // si no existe la ruta se rechaza la promesa
-  reject('La ruta no existe');
+    reject('La ruta no existe');
   }
- 
- });
-};
+});
 
 module.exports = {
-  mdLinks
+  mdLinks,
 };
