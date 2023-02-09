d9cdb4be46f9a1dd928944a07a502a6db5a05ee8
Carolina Pacori <carolina.pacori@gmail.com>
2023-02-09 00:22:40 -0500

    funcion read file, devuelve links en array

--- Api.js
+++ Api.js
@@ -1,28 +1,59 @@
 const fs = require('fs');
 const path = require('path');
+// const axios = require('axios');
+
 // Funcion saber si existe una ruta
-const pathExists = (inputPath) => fs.existsSync(path);
+const pathExists = (inputPath) => fs.existsSync(inputPath);
 
 // Funcion identificar una ruta absoluta
 const isAbsolutePath = (inputPath) => path.isAbsolute(inputPath);
 // prueba
-console.log(isAbsolutePath('/caro/caro/Desktop/Laboratoria/DEV001-md-links/NONES/prueba.md'));
+// console.log(isAbsolutePath('/MD LINKS/DEV001-md-links/README.md'));
 
 // Funcion convertir a absoluta
 const convertAbsolutePath = (inputPath) => (isAbsolutePath(inputPath) ? inputPath
   : path.resolve(inputPath));
   // prueba
- console.log(convertAbsolutePath('prueba/pruebita.md'));
+// console.log(convertAbsolutePath('prueba/pruebita.md'));
 
-// Identificar si es un archivo o directorio.(solo archivo)
+// Identificar si es un archivo (solo archivo)
+const isItFile = (inputPath) => fs.statSync(inputPath).isFile();
+// prueba
+// console.log(isItFile('README.md'));
 
 // Identificar si tiene formato md.
 const isMarkdown = (inputPath) => path.extname(inputPath) === '.md';
+// prueba
+// console.log(isMarkdown('README.md'));
+
 // Leer cada archivo del arreglo.
+// leer el archivo md
+const readFile = (inputPath) => fs.readFileSync(inputPath, 'utf8');
+
+const findLinks = (content, inputPath) => {
+  const regExp = /\[(.+)\]\((https?:\/\/.+)\)/gi;
+  const arrayLinks = [...content.matchAll(regExp)];
+  console.log(arrayLinks);
+  const arrayObjects = [];
+  console.log(arrayObjects);
+  for (let i = 0; i < arrayLinks.length; i++) {
+    arrayObjects.push({
+      href: arrayLinks[i][2],
+      text: arrayLinks[i][1],
+      file: inputPath,
+    });
+    // arrayObjects.forEach((linkes) => console.log(linkes));
+  }
+  return arrayObjects;
+};
+console.log(findLinks(readFile('README.md'), 'README.md'));
 
 module.exports = {
   pathExists,
   isAbsolutePath,
   convertAbsolutePath,
+  isItFile,
   isMarkdown,
+  readFile,
+  findLinks,
 };
--- index.js
+++ index.js
@@ -2,14 +2,13 @@ const {
   pathExists,
   isAbsolutePath,
   convertAbsolutePath,
+  isItFile,
   isMarkdown,
+  readFile,
+  findLinks,
 } = require('./Api');
 
 const mdLinks = (inputPath, options) => new Promise((resolve, reject) => {
-  // si existe resolver la promesa
-  // return reject('si existe la ruta');
-  // }
-
   // identifica si la ruta existe.
   if (pathExists(inputPath)) {
     console.log('La ruta existe');
@@ -19,13 +18,26 @@ const mdLinks = (inputPath, options) => new Promise((resolve, reject) => {
       // Convertir a una ruta absoluta
       const pathAbsolute = convertAbsolutePath(inputPath);
       console.log(`La ruta relativa se convirtio a ABSOLUTA ${pathAbsolute}`);
-      // Probar si esa ruta absoluta es un archivo (o un directorio CREO Q NO)
-      // (Si es un directorio filtrar los archivos md.(CREO Q NO VA))
-      // ver si es una archivo .md
-      // Si es un archivo .md devuelve un arreglo
+      // Probar si esa ruta absoluta es un archivo
+      if (isItFile(inputPath)) {
+        console.log('Es un archivo');
+        // (Si es un directorio filtrar los archivos md.(CREO Q NO VA))
+        // ver si es una archivo .md
+        if (isMarkdown(inputPath)) {
+          console.log('Es un archivo markdown');
+          const content = readFile(inputPath);
+          if (content !== '') {
+            const arrayLinks = findLinks(content, inputPath);
+            // si encuentra archivos md, crear un arreglo de mds
+            // console.log(arrayLinks);
+            arrayLinks.forEach((linkes) => console.log(linkes));
+          }
+        }
+      }
     }
   }
 });
+  // Si es un archivo .md devuelve un arreglo
 
 module.exports = {
   mdLinks,
